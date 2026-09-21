0db2251bd5d39a11407e3c8d5ee9e51a27e9d8d1
valeledesma19 <valentinaledesma2407@gmail.com>
2026-09-21 19:20:30 -0300

    Setup inicial backend: Spring Boot + Security base

---
+++ vidaanimal-backend/src/main/java/com/vidaanimal/vidaanimal_backend/VidaanimalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vidaanimal.vidaanimal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaanimalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidaanimalBackendApplication.class, args);
+	}
+
+}
---
+++ vidaanimal-backend/src/test/java/com/vidaanimal/vidaanimal_backend/VidaanimalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidaanimal.vidaanimal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaanimalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
